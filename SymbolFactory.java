9a1cebd89c0d7c1a12028f7fd14c34bd5cbc6f0e
sglickfield <sglickfield@users.noreply.github.com>
2014-12-03 17:01:40 -0500

    Create SymbolFactory.java

---
+++ SymbolFactory.java
@@ -0,0 +1,25 @@
+class SymbolFactory
+{
+    public static Symbol getSymbol(String input)
+    {
+        if (isNumber(input))
+        {
+           return new Number(Integer.parseInt(input)); 
+        }
+        else if (isRoman(input))
+        {
+           return new Roman(input); 
+        }
+        return new InvalidSymbol(input);
+    }
+    
+    private static boolean isNumber(String str)
+    {
+        return (str.matches("\\d+") && Integer.parseInt(str) < 4000);
+    }
+
+    private static boolean isRoman(String str)
+    {
+        return str.matches("^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$");
+    }
+}
